64b89165512694ede31d49399fbf1666b054b482
Lautaro-Espinosa015 <espinosalautii@outlook.com>
2026-01-27 19:39:25 -0300

    Dialogos y moviemientos funcionales

--- Assets/Materiales/Scripts/Dialogue.cs
+++ Assets/Materiales/Scripts/Dialogue.cs
@@ -1,11 +1,46 @@
 using UnityEngine;
 using System.Collections;
+using TMPro;
 public class Dialogue : MonoBehaviour
 {
+    [SerializeField] private GameObject dialogueMark;
+    [SerializeField] private GameObject dialoguePanel;
+    [SerializeField] private TMP_Text dialogueText;
+    [SerializeField, TextArea(4, 6)] private string[] dialogueLines;
+
+    private float typingTime = 0.05f;
+    private bool didDialogueStart;
     private bool isPlayerInRange;
+    private int lineIndex;
     void Update()
     {
-        
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.F))
+        {
+            if (!didDialogueStart)
+            {
+                StartDialogue();
+            }
+            
+        }
+    }
+
+    private IEnumerator ShowLine()
+    {
+        dialogueText.text = string.Empty;
+
+        foreach(char ch in dialogueLines[lineIndex])
+        {
+            dialogueText.text += ch;
+            yield return new WaitForSeconds(typingTime);
+        }
+    }
+    private void StartDialogue()
+    {
+        didDialogueStart = true;
+        dialoguePanel.SetActive(true);
+        dialogueMark.SetActive(true);
+        lineIndex = 0;
+        StartCoroutine(ShowLine());
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -13,6 +48,7 @@ public class Dialogue : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = true;
+            dialogueMark.SetActive(true);
             Debug.Log("Se puede hablar");
         }
         
@@ -23,6 +59,8 @@ public class Dialogue : MonoBehaviour
         if (collision.gameObject.CompareTag("Player"))
         {
             isPlayerInRange = false;
+            dialogueMark.SetActive(false);
+            Debug.Log("No se puede hablar");
         }
         }
 }
