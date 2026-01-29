1ccfff53932f122c6723b26670ded9be4ee9083e
Lautaro-Espinosa015 <espinosalautii@outlook.com>
2026-01-29 00:05:48 -0300

    Mapa de Brian con colisiones + sistema de decisiones

---
+++ Assets/Materiales/Ambiente/Ambiente/Chicas/scrip/CHica.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+
+{
+
+    public float speed = 5f;
+
+    private Rigidbody2D rd2D;
+    private Vector2 movementInput;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rd2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInput.x = Input.GetAxisRaw("Horizontal");
+        movementInput.y = Input.GetAxisRaw("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        rd2D.linearVelocity = movementInput * speed;
+    }
+}
---
+++ Assets/Materiales/Scripts/Camara.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Camara : MonoBehaviour
+{
+    public Transform elPersonaje; //Elejir el personaje 
+    public float suavizado = 5f;  //PAra que la Camara se vas rapida,suabe o mas lento
+    public float distanciaZ = -15f; // Qué tan atrás está la cámara
+
+    void LateUpdate()
+    {
+        // Adonde el personaje esta llendo
+        // dirección que usa la X y la Y del personaje distancia Z
+        Vector3 destino = new Vector3(elPersonaje.position.x, elPersonaje.position.y, distanciaZ);
+
+        // 2. Movemos la cámara desde donde está hacia el "destino" poco a poco
+        transform.position = Vector3.Lerp(transform.position, destino, suavizado * Time.deltaTime);
+    }
+}
